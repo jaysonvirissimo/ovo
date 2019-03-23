0903ad8c8c6e1714bd5b9ab9557c2e39f35a4924
Jayson Virissimo <jayson.virissimo@asu.edu>
2019-03-22 17:07:22 -0700

    Quick and dirty parse to syntax tree

--- spec/ovo_spec.rb
+++ spec/ovo_spec.rb
@@ -3,6 +3,25 @@ RSpec.describe Ovo do
     expect(Ovo::VERSION).to be
   end
 
+  describe '.parse' do
+    context 'with a simple expression' do
+      let(:arguments) { syntax_tree[:args] }
+      let(:first_argument) { arguments.first }
+      let(:operator) { syntax_tree[:operator] }
+      let(:program) { '+(a, 10)' }
+      let(:second_argument) { arguments.last }
+      let(:syntax_tree) { described_class.parse(program) }
+
+      it 'builds the proper syntax tree' do
+        expect(syntax_tree[:type]).to eq('apply')
+        expect(operator[:type]).to eq('word')
+        expect(operator[:value]).to eq('+')
+        expect(first_argument[:type]).to eq('word')
+        expect(first_argument[:value]).to eq('a')
+      end
+    end
+  end
+
   describe ".run" do
     let(:program) do
       <<~PROGRAM
