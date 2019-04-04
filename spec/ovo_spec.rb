4194114d416e6e2462615b36513d97a1e2387b7e
Jayson Virissimo <jayson.virissimo@asu.edu>
2019-04-03 22:40:55 -0700

    Add simpler failing test

--- spec/ovo_spec.rb
+++ spec/ovo_spec.rb
@@ -4,17 +4,27 @@ RSpec.describe Ovo do
   end
 
   describe ".run" do
-    let(:program) do
-      <<~PROGRAM
-        do(define(total, 0),
-          define(count, 1),
-          while(<(count, 11),
-                do(define(total, +(total, count)),
-                   define(count, +(count, 1)))),
-          print(total))
-      PROGRAM
+    subject { described_class.run(program) }
+
+    context 'with a program of a single expression' do
+      let(:program) { 'print(42)' }
+
+      it { expect(subject).to eq(42)}
     end
 
-    it { expect(described_class.run(program)).to eq(55) }
+    context 'with a non-trivial program' do
+      let(:program) do
+        <<~PROGRAM
+          do(define(total, 0),
+            define(count, 1),
+            while(<(count, 11),
+                  do(define(total, +(total, count)),
+                     define(count, +(count, 1)))),
+            print(total))
+        PROGRAM
+      end
+
+      it { expect(subject).to eq(55) }
+    end
   end
 end
