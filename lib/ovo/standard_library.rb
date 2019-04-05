36bc4b66933ac2927e6f1b86ceb45164a4533163
Jayson Virissimo <jayson.virissimo@asu.edu>
2019-04-04 23:43:46 -0700

    It works now...it bloody works!
    Standard library handles references and values.

--- lib/ovo/standard_library.rb
+++ lib/ovo/standard_library.rb
@@ -2,7 +2,18 @@ module Ovo
   module StandardLibrary
     ['+', '-', '>', '<', '*', '/', '%'].each do |operator|
       define_method(operator) do |arguments|
-        arguments[0].send(operator, arguments[1])
+        a = if arguments[0][:type] == 'value'
+              arguments[0][:value]
+            else
+              send(arguments[0].fetch(:name))
+        end
+        b = if arguments[1][:type] == 'value'
+              arguments[1][:value]
+            else
+              send(arguments[1].fetch(:name))
+        end
+
+        a.send(operator, b)
       end
     end
   end
